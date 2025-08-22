ff8ab766042df8c98e176e0767d89c2c08a1e8b5
nahiaraadrover <nahiaraaadrover@gmail.com>
2025-08-22 17:16:47 -0300

    Ej 1.1 react hecho

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
 
-function App() {
+  const Header = ({ course }) => {
+    return <h1>{course}</h1>;
+  };
+
+  const Content = ({ part, exercises }) => {
+    return (
+      <p>
+        {part} - Numero de ejericios: {exercises}
+      </p>
+    );
+  };
+
+  const Total = ({ a1, a2, a3 }) => {
+    return <p>Number of exercises: {a1 + a2 + a3}</p>;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course} />
+
+      <Content part={part1} exercises={exercises1} />
+
+      <Content part={part2} exercises={exercises2} />
+
+      <Content part={part3} exercises={exercises3} />
+
+      <Total a1={exercises1} a2={exercises2} a3={exercises3} />
     </div>
   );
-}
+};
 
 export default App;
